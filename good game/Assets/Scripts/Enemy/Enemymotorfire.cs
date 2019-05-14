a84badcb305f501b9ec72892d80071cb12a34431
AlexLN01 <Alex.ln@telia.com>
2019-05-14 11:03:09 +0200

    ww
    egeg

--- good game/Assets/Scripts/Enemy/Enemymotorfire.cs
+++ good game/Assets/Scripts/Enemy/Enemymotorfire.cs
@@ -39,6 +39,7 @@ public class Enemymotorfire : MonoBehaviour
     private void Fire(Vector3 direction)
     {
         Debug.Log("Fire!!!");
+        Debug.Log(gameObject);
         GameObject las = Instantiate(LaserPrefab, transform.position, Quaternion.LookRotation(direction));
         las.SetActive(true);
         las.transform.rotation = Quaternion.LookRotation(new Vector3(0f, 90f, 0f));
