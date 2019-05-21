8e7aed2dc96faf22b387ca4c1d5e09bde40b4f9b
AlexLN01 <Alex.ln@telia.com>
2019-05-21 08:45:32 +0200

    fewf
    egegeg

--- good game/Assets/Scripts/Enemy/Enemymotor.cs
+++ good game/Assets/Scripts/Enemy/Enemymotor.cs
@@ -5,34 +5,34 @@ using UnityEngine;
 public class Enemymotor : MonoBehaviour
 {
 
-    public float Speed;
-    public float AngualarSpeeed;
-    public AudioClip ExplosionAudioClip;
+    public float Speed; //gör en flott som gör en speed 
+    public float AngualarSpeeed; //snurra med hastighet
+    public AudioClip ExplosionAudioClip; //gör ett ljud som man kan lägga till
     
-    private Vector3 direction; 
+    private Vector3 direction; //gör så att du kan röra dig i en direction 
 
     
     void Start()
     {
-        direction = new Vector3(Random.Range(-100, 100), 0, Random.Range(-100, 100)).normalized;
+        direction = new Vector3(Random.Range(-100, 100), 0, Random.Range(-100, 100)).normalized; //vilket håll den ska röra sig åt
     }
 
     
     void Update()
     {
-       transform.Rotate(Vector3.up, AngualarSpeeed * Time.deltaTime); 
-       transform.Translate(direction * Speed * Time.deltaTime, Space.World);
+       transform.Rotate(Vector3.up, AngualarSpeeed * Time.deltaTime); //gör så att object kan rotera och snurra
+       transform.Translate(direction * Speed * Time.deltaTime, Space.World); //gör så att du kan röra dig i en directions plus speed i världen
     }
 
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other) // denna gör så när något går ihop med lasern så kommer något att hända
     {
-        if(other.tag == "laser")
+        if(other.tag == "laser") // om laser träffa så kommer det som stå under att hända
         {
-            Destroy(other.gameObject);
+            Destroy(other.gameObject); // gör så att när lasern träffar så går objektet sönder
             //CreateAudioHelper.CreateAudioGameObject(ExplosionAudioClip, "EnemyDestroySound", transform.position);
 
-            Destroy(this.gameObject);
+            Destroy(this.gameObject); //förstör detta ojektet
         }
     }
 
--- good game/Assets/Scripts/randomspawn.cs
+++ good game/Assets/Scripts/randomspawn.cs
@@ -3,21 +3,21 @@
  
  public class randomspawn : MonoBehaviour {
  
-     public GameObject coinDrop;
-     public float timer = 0f;
-     public float freq = 0.1f;
-     public Vector3 position;
+     public GameObject coinDrop; //gör ett objekt 
+     public float timer = 0f; // gör en timer
+     public float freq = 0.1f; //skapar en float
+     public Vector3 position; // spawnar på en position
 
 
      void Update() 
      {
-         if(timer <= 0)
+         if(timer <= 0) //om tiden är mindre än noll
          {
-             position = transform.position + new Vector3(Random.Range(-50.0F, 50.0F), 0, Random.Range(-50.0F, 50.0F));
-             Instantiate(coinDrop, position, Quaternion.identity);
-             timer = freq;
+             position = transform.position + new Vector3(Random.Range(-50.0F, 50.0F), 0, Random.Range(-50.0F, 50.0F)); // positionen är lika med olika posistionen plus new vector och dem spawnars random mellan dessa kodinatorna
+             Instantiate(coinDrop, position, Quaternion.identity); //gör en clone och spawnerna på den positionen
+             timer = freq; //timer lika med 0,1
          } else {
-             timer -= Time.deltaTime;
+             timer -= Time.deltaTime; //tiden är lika med 
          }
      }
 
